409e6b0241b7254a8509b6a44c64ee5273f6f1d1
imollov <ivomollov@gmail.com>
2024-01-30 13:04:53 +0200

    feat: check upkeep gas limit

--- src/lib/checks.ts
+++ src/lib/checks.ts
@@ -1,5 +1,6 @@
 import { KeeperRegistryContractConfig } from '@/config/contracts'
 import { createPublicClient, http, parseAbi } from 'viem'
+import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts'
 
 export const checkUpkeepFunction = async (
   upkeepId: string,
@@ -118,6 +119,96 @@ export const checkUpkeepPerfromFunction = async (
   }
 }
 
+export const checkUpkeepGasLimit = async (
+  upkeepId: string,
+  rpcUrl: string | undefined,
+) => {
+  const client = createPublicClient({
+    transport: http(rpcUrl),
+  })
+
+  // Get the target contract
+  const data = await client.readContract({
+    address: KeeperRegistryContractConfig.address,
+    abi: KeeperRegistryContractConfig.abi,
+    functionName: 'getUpkeep',
+    args: [BigInt(upkeepId)],
+  })
+
+  const upkeepContract = data.target
+  const upkeepCheckData = data.checkData
+  const performGasLimit = data.performGas
+
+  // Get the perform data
+  let checkSimulation
+  try {
+    checkSimulation = await client.simulateContract({
+      address: upkeepContract,
+      abi: parseAbi([
+        'function checkUpkeep(bytes calldata checkData) external returns (bool upkeepNeeded, bytes memory performData)',
+      ]),
+      functionName: 'checkUpkeep',
+      args: [upkeepCheckData],
+    })
+  } catch (e) {
+    console.log(e) // TODO: extract error message
+  }
+
+  if (!checkSimulation?.result) {
+    return {
+      status: 'Fail',
+      details: 'The check function is reverting and preventing the simulation',
+    }
+  }
+
+  // Simulate the perform function
+  const performData = checkSimulation?.result[1] as `0x${string}`
+
+  let performSimulationSuccess = false
+  let gasEstimate = 0
+  try {
+    const result = await client.estimateContractGas({
+      address: upkeepContract,
+      abi: parseAbi([
+        'function performUpkeep(bytes calldata performData) external',
+      ]),
+      functionName: 'performUpkeep',
+      args: [performData],
+      account: generateRandomAddress(),
+    })
+    gasEstimate = Number(result)
+    performSimulationSuccess = true
+  } catch (e) {
+    console.log(e) // TODO: extract error message
+  }
+
+  if (!performSimulationSuccess) {
+    return {
+      status: 'Fail',
+      details:
+        'The perform function is reverting and preventing the simulation',
+    }
+  }
+
+  if (gasEstimate > performGasLimit) {
+    return {
+      status: 'Fail',
+      details: `The perform function is using more gas than the specified gas limit. The gas limit is ${performGasLimit} and the gas estimate is ${gasEstimate}`,
+    }
+  } else {
+    return {
+      status: 'Pass',
+      details: 'No issues found',
+    }
+  }
+}
+
+const generateRandomAddress = () => {
+  const privateKey = generatePrivateKey()
+  const account = privateKeyToAccount(privateKey)
+  return account.address
+}
+
 export const checks = [
   {
     name: 'Upkeep check function',
@@ -126,5 +217,9 @@ export const checks = [
   {
     name: 'Upkeep perform function',
     fn: checkUpkeepPerfromFunction,
+  },
+  {
+    name: 'Upkeep gas limit',
+    fn: checkUpkeepGasLimit,
   },
 ]
