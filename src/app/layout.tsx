ef5d445683a7ea36031f671d07ea265bbcd97635
imollov <ivomollov@gmail.com>
2024-03-15 10:39:26 +0200

    feat: add matomo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 
+import Script from 'next/script'
 import { siteConfig } from '@/config/site'
 import { fontSans, fontHeading } from '@/lib/fonts'
 import { cn } from '@/lib/utils'
@@ -49,6 +50,21 @@ export default function RootLayout({
             <SiteHeader />
             <div className="flex-1">{children}</div>
             <SiteFooter />
+            <Script id="matomo">
+              {`
+              var _paq = window._paq = window._paq || [];
+              /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="https://tomato.apps.hack.bg/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '4']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+              })();
+              `}
+            </Script>
           </div>
         </Providers>
       </body>
